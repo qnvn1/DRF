174b95e14c7f24fe0ea751835b82948f05f55e5f
Dan Ivan E. Labin <danivanlabin@gmail.com>
2026-02-25 22:41:40 +0800

    uppp

--- school_management/settings.py
+++ school_management/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1", "https://drf-3ajp.onrender.com").split(",")
 
 # APPLICATIONS
 INSTALLED_APPS = [
