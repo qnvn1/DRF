71c8573e357e6d5271ff42a7235e18322ba2a4ab
Dan Ivan E. Labin <danivanlabin@gmail.com>
2026-02-25 23:36:40 +0800

    commit

--- create_admin.py
+++ create_admin.py
@@ -6,11 +6,14 @@ django.setup()
 
 from django.contrib.auth.models import User
 
+# Admin credentials
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL", "admin@example.com")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin")
 
-if not User.objects.filter(username="admin").exists():
+if not User.objects.filter(username=ADMIN_USERNAME).exists():
     User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
+        username=ADMIN_USERNAME,
+        email=ADMIN_EMAIL,
         password=ADMIN_PASSWORD
     )
